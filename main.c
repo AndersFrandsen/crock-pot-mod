f8ef7764de753fe235a6b5891e1dde060c907d59
Anders Frandsen <anfran@anfran.dk>
2021-04-22 18:31:47 +0200

    refac: code format

--- main.c
+++ main.c
@@ -1,4 +1,4 @@
-#include "configuration_bits.c"
+//#include "configuration_bits.c"
 #include "xc.h"
 #include "ds18b20.h"
 
@@ -9,34 +9,34 @@
 #define CONVERT_T 0x44
 #define READ_SCRATCHPAD 0xBE
 
-#define FCY 32000000UL/16
+#define FCY 32000000UL / 16
 #include <libpic30.h>
 
 void main(void)
-{    
+{
     ANSBbits.ANSB2 = 0;
     ONE_WIRE_TRIS = 1;
-    
+
     unsigned char i;
     unsigned char scratchpad[9];
-    
+
     ow_reset_pulse();
     ow_write_byte(SKIP_ROM);
     ow_write_byte(CONVERT_T);
-    
-    while (!read_ow())
+
+    while(!read_ow())
     {
         // Wait for ds18b20 to finish temperature conversion
     }
-    
+
     ow_reset_pulse();
     ow_write_byte(SKIP_ROM);
     ow_write_byte(READ_SCRATCHPAD);
-    
-    for (i = 0; i < 9; i++)
+
+    for(i = 0; i < 9; i++)
         scratchpad[i] = ow_read_byte();
-    
-    while(1)
+
+    while (1)
     {
     }
 }
